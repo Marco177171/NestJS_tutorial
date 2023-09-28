d13d4dcd0a217740e3af3c9c16a8f160e3097e85
Marco Sebastiani <masebast@e4r13p4.42roma.it>
2023-09-28 19:28:23 +0200

    check INestApplication in tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,23 @@
-import { Test } from '@nestjs/testing'
+import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import { ValidationPipe, INestApplication } from '@nestjs/common';
 
 describe('App e2e', () => {
+  let app = ;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile()
+    }).compile();
+    const app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+    await app.init();
   });
+  afterAll(async () => {
+    app.close();
+  })
   it.todo('should pass');
 });
