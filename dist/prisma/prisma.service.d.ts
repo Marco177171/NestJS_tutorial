e40d479efee381a2c09e05c0859400afdf82babe
Marco Sebastiani <masebast@e4r13p1.42roma.it>
2023-09-29 15:37:14 +0200

    testing with jest -> check why INestApplication doesn't work

--- dist/prisma/prisma.service.d.ts
+++ dist/prisma/prisma.service.d.ts
@@ -2,4 +2,5 @@ import { PrismaClient } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
 export declare class PrismaService extends PrismaClient {
     constructor(config: ConfigService);
+    cleanDb(): Promise<[import(".prisma/client").Prisma.BatchPayload, import(".prisma/client").Prisma.BatchPayload]>;
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,14 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { ValidationPipe, INestApplication } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
+import * as pactum from 'pactum';
+import { AuthDto } from '../src/auth/dto';
 
 describe('App e2e', () => {
-  let app = ;
+  let app: INestApplication;
+  let prisma: PrismaService;
+  
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -12,12 +17,60 @@ describe('App e2e', () => {
     app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
-      }),
+      })
     );
     await app.init();
+    await app.listen(3000);
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
+  
   afterAll(async () => {
     app.close();
-  })
-  it.todo('should pass');
+  });
+  
+  describe ('Auth', () => {
+    const dto: AuthDto = {
+      email: 'jane@gmail.com',
+      password: 'pass123'
+    }
+    describe('Signup', () => {
+      it('should throw if email empty', () => {
+        return pactum.spec().post('/auth/signup').withBody({password: dto.password}).expectStatus(400);
+      });
+      it('should sign up', () => {
+        return pactum.spec().post('/auth/signup').withBody(dto).expectStatus(201);
+      });
+    });
+    describe('Signin', () => {
+      it.todo('should sign in');
+    });
+  });
+
+  describe ('User', () => {
+    describe('Get me', () => {
+
+    });
+    describe('Edit user', () => {
+
+    });
+  });
+
+  describe ('Boookmarks', () => {
+    describe('Create Bookmark', () => {
+
+    });
+    describe('Get Bookmarks', () => {
+
+    });
+    describe('Get Bookmark By id', () => {
+
+    });
+    describe('Edit Bookmark', () => {
+
+    });
+    describe('Delete Bookmark', () => {
+
+    });
+  });
 });
